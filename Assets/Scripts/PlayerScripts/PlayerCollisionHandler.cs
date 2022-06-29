cbfbfa9c96b5325c4398c8d8c9a770f772cea2c4
Zach Callanan <zach.callanan@yousician.com>
2022-06-29 21:26:38 +0300

    PlayerCollisionHandler dispose

--- Assets/Scripts/PlayerScripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerScripts/PlayerCollisionHandler.cs
@@ -1,26 +1,53 @@
+using System;
+using Misc;
 using UniRx;
 using UniRx.Triggers;
 using Zenject;
 
 namespace PlayerScripts
 {
-    public class PlayerCollisionHandler : IInitializable
+    public class PlayerCollisionHandler : IInitializable, IDisposable
     {
         private readonly Player _player;
+        private readonly GameState _gameState;
+        
+        private readonly CompositeDisposable _disposables = new CompositeDisposable();
 
-        public PlayerCollisionHandler(Player player)
+        public PlayerCollisionHandler(
+            Player player,
+            GameState gameState)
         {
             _player = player;
+            _gameState = gameState;
         }
         
         public void Initialize()
         {
-            _player.GameObj
-                .OnTriggerEnterAsObservable()
-                .Subscribe(_ => HandleCollision());
+            HandleCollisionOnTriggerEnter();
+        }
+        
+        public void Dispose()
+        {
+            _gameState.CurrentLives
+                .Subscribe(lives =>
+                {
+                    if (lives < 0)
+                    {
+                        _disposables.Clear();
+                    }
+                })
+                .AddTo(_disposables);
         }
 
-        private void HandleCollision()
+        private void HandleCollisionOnTriggerEnter()
+        {
+            _player.GameObj
+                .OnTriggerEnterAsObservable()
+                .Subscribe(_ => SetupPlayerDeathState())
+                .AddTo(_disposables);
+        }
+
+        private void SetupPlayerDeathState()
         {
             _player.MeshRenderer.enabled = false;
             _player.MeshCollider.enabled = false;
